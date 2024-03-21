0ec1500453e408ededd8029a0274e600397ac0c5
azharali3 <azharaliujjan77@gmail.com>
2024-03-21 10:47:15 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,3 @@
+var quote = "Nothing is impossible because itself I AM POSSIBLE";
+var author = "Hina Naseer Ahmed";
+console.log(" \"".concat(quote, "\"  - ").concat(author));
---
+++ assignment 3/main.ts
@@ -0,0 +1,3 @@
+let quote:string = "Nothing is impossible because itself I AM POSSIBLE"
+let author:string = "Hina Naseer Ahmed"
+console.log(` "${quote}"  - ${author}`)
